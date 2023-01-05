5d02a04d0e0a92cb378368dde6b26370ca8ca13b
Naviralobo <majel.josephine>
2023-01-05 18:45:01 +0530

    creating a html page with different url

--- first-app.js
+++ first-app.js
@@ -1,6 +1,24 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Navira");
+  const url = req.url;
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  if (url === "/home") {
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (url === "/about") {
+    res.write("<body><h1>Welcome to About us page</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (url === "/node") {
+    res.write("<body><h1>Welcome to my node-js </h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
-server.listen(4000);
+server.listen(3000);
