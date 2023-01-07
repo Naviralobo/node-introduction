5c8c1679f5096c1c306b8f2280eb9ae96eae5086
Naviralobo <majel.josephine>
2023-01-07 11:29:49 +0530

    reading text from file and displaying in UI

--- first-app.js
+++ first-app.js
@@ -1,24 +1,45 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    return fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      // res.setHeader("Content-Type", "text/html");
+      res.write("<html>");
+      res.write("<head><title>My First Page</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        "<body><form action='/message' method='post'><input type='text' name='message'/><button type='submit'>send</button></form> </body>"
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString(); //outputs message=typedvalue in input,since message is the name given to input and it outputs as key,value pairs
+      const message = parsedBody.split("=")[1]; //therefore take the value by splitting it
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
-  if (url === "/home") {
-    res.write("<body><h1>Welcome home</h1></body>");
-    res.write("</html>");
-    res.end();
-  }
-  if (url === "/about") {
-    res.write("<body><h1>Welcome to About us page</h1></body>");
-    res.write("</html>");
-    res.end();
-  }
-  if (url === "/node") {
-    res.write("<body><h1>Welcome to my node-js </h1></body>");
-    res.write("</html>");
-    res.end();
-  }
+  res.write("<body><h1>Welcome to my node-js </h1></body>");
+  res.write("</html>");
+  return res.end();
 });
-server.listen(3000);
+server.listen(3001);
