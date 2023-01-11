6d127497d1ace7e04e4e95fa10f98b35607f7b5f
Naviralobo <majel.josephine>
2023-01-11 16:00:50 +0530

    routes and filtering routes

--- first-app.js
+++ first-app.js
@@ -2,20 +2,16 @@ const http = require("http");
 const express = require("express"); //express package returns a ffunction
 const bodyParser = require("body-parser");
 const app = express();
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><input type='text' name='size'><button type='submit'>Add product</button></form>"
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello from express</h1>");
-});
 // const server = http.createServer(app);
 // server.listen(3000);------------------------>These line can be converted to app.listen(3000)
 app.listen(3002);
