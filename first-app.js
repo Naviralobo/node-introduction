726eef7265c154a85166d245826024953ba23cbd
Naviralobo <majel.josephine>
2023-01-11 12:49:18 +0530

    middlewares and listen

--- first-app.js
+++ first-app.js
@@ -1,5 +1,16 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express"); //express package returns a ffunction
+const app = express();
 
-const server = http.createServer(routes.handler);
-server.listen(3001);
+app.use((req, res, next) => {
+  console.log("first middleware");
+  next();
+});
+app.use((req, res, next) => {
+  //   console.log("second middleware");
+  res.send("<h1>hello from express</h1>");
+  res.send({ name: "navi" });
+});
+// const server = http.createServer(app);
+// server.listen(3000);------------------------>These line can be converted to app.listen(3000)
+app.listen(3000);
