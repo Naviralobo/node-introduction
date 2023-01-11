c4a96d09a30762451eea4b2f5a1320808c935b1f
Naviralobo <majel.josephine>
2023-01-11 14:56:53 +0530

    body-parser

--- first-app.js
+++ first-app.js
@@ -1,16 +1,21 @@
 const http = require("http");
 const express = require("express"); //express package returns a ffunction
+const bodyParser = require("body-parser");
 const app = express();
-
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><input type='text' name='size'><button type='submit'>Add product</button></form>"
+  );
 });
-app.use((req, res, next) => {
-  //   console.log("second middleware");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>hello from express</h1>");
-  res.send({ name: "navi" });
 });
 // const server = http.createServer(app);
 // server.listen(3000);------------------------>These line can be converted to app.listen(3000)
-app.listen(3000);
+app.listen(3002);
